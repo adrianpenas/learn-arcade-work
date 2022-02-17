ef32a68d5eb24ebf6b57b95ec16cd22624b6bae0
adrian.penas <adrian.penas@edu.uah.es>
2022-02-17 15:41:19 +0100

    Hola

---
+++ Testing/test.py
@@ -0,0 +1,4 @@
+print('Hello world')
+print("It was a dark and stormy night.")
+print("Suddenly a shot rang out!")
+print("Audrey Hepburn once said \"Nothing is impossible. The word itself says 'I'm Possible!'.\"")
